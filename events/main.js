e9615d98f45468f80334d566392081777c7762cc
Mayumi EMORI <mayumi.emori@gmail.com>
2022-07-27 18:59:41 +0900

    Revert "Tokyo 14th イベントページをJPに移動"

--- events/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  if( $.isFunction( $.fn.fancybox ) ) {
-    $("#inline").fancybox({
-      'hideOnContentClick': false,
-      'height': 600,
-      'width': 800
-    });
-  }
-});
